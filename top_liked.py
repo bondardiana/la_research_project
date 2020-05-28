a79c9b547b400279f5af3102e92b9de850d78722
Diana Bondar <45092495+bondardiana@users.noreply.github.com>
2020-05-28 21:33:17 +0300

    Update top_liked.py

--- top_liked.py
+++ top_liked.py
@@ -2,10 +2,14 @@
 
 
 def top_liked_tweets(twits, favs, user):
-
     favs_copy = favs.copy()
     print(favs_copy)
+    result = []
     for i in range(11):
-        print(i, user[favs_copy.index(max(favs_copy))],
-              ':', twits[favs_copy.index(max(favs_copy))])
-        favs_copy[favs_copy.index(max(favs_copy))] = 0
+        ind = favs_copy.index(max(favs_copy))
+        result.append([i, user[ind],
+                       ':', twits[ind], favs_copy[ind]])
+        print(i, user[ind],
+              ':', twits[ind], favs_copy[ind])
+        favs_copy[ind] = 0
+    return result
