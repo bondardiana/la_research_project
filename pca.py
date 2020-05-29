645e639763d44b7c675076a1bf83e3286e47a294
Diana Bondar <45092495+bondardiana@users.noreply.github.com>
2020-05-29 14:35:13 +0300

    Update pca.py

--- pca.py
+++ pca.py
@@ -1,36 +1,35 @@
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
 import numpy as np
-from matr import matr_mult
+from sklearn.decomposition import PCA
+import pandas as pd
 
 
-def matr_pca(A):
-    # Take the whole dataset consisting of d+1 dimensions, scale it
-    # and ignore the labels such that our new dataset becomes d dimensional.
-
-    A_scaled = StandardScaler().fit_transform(np.array(A))
-
-    # Compute the mean for every dimension of the whole dataset.
+def pca1(A, n_components=2):
+    # Compute the mean for every dimension of the whole dataset, mean centering
     A_mean = np.mean(A, axis=0)
-
+    A = A - A_mean
     # Compute the covariance matrix of the whole dataset.
-    A_scaled_tr = A_scaled.T
-    covariance_matrix = np.cov(A_scaled_tr)
+    cov_matrix = np.cov(A.T)
     # Compute eigenvectors and the corresponding eigenvalues.
-    eig_vals, eig_vecs = np.linalg.eig(covariance_matrix)
-    # k eigenvectors with the largest eigenvalues to form a d × k dimensional matrix
-    zipped_lists = zip(eig_vals.real, eig_vecs)
-    sorted_zipped_lists = sorted(zipped_lists)
-    W = [element for _, element in sorted_zipped_lists]
-    # Use this d × k eigenvector matrix to transform the samples onto the new subspace.
-    result = pd.DataFrame(columns=['PC1', 'PC2'])
-    result['PC1'] = A_scaled.dot(np.array(W).T[0])
-    result['PC2'] = A_scaled.dot(np.array(W).T[1])
-    return result
+    eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
+    eigenvectors = eigenvectors.T
+    # sort and take n eigenvectors with the largest eigenvalues to form a d × n dimensional matrix
+    idxs = np.argsort(eigenvalues)[::-1]
+    eigenvalues = eigenvalues[idxs]
+    eigenvectors = eigenvectors[idxs]
+    # store first n eigenvectors
+    components = eigenvectors[0:n_components]
+
+    return np.dot(A, components.T)
 
 
 if __name__ == '__main__':
     A = [[1, 2, 5],
          [3, 5, 6],
          [7, 8, 10]]
-    print(matr_pca(A))
+    print(pca1(A))
+    X = np.array(A)
+    pca = PCA(n_components=2)
+    principalComponents = pca.fit_transform(X)
+    principalDf = pd.DataFrame(data=principalComponents, columns=[
+                               'principal component 1', 'principal component 2'])
+    print(principalDf)
